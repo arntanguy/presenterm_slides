50c1f67a4a41d8168f69e4a4e567e7a20c95c2e1
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-07-09 16:26:40 +0200

    flake: add export to html

--- flake.nix
+++ flake.nix
@@ -9,17 +9,19 @@
       { ... }:
       let
         makeSlidePackage =
-          slideName:
+          slideName: html:
           {
             stdenv,
             presenterm,
             kitty,
             lib,
+            buildHtml ? html,
             ...
           }:
           stdenv.mkDerivation {
             name = slideName;
             version = "1.0.0";
+            outputs = [ "out" ] ++ lib.optional buildHtml [ "html" ];
             nativeBuildInputs = [
               presenterm
               kitty
@@ -33,16 +35,33 @@
               exec ${kitty}/bin/kitty ${presenterm}/bin/presenterm -x "$out/share/slides/slides.md" "\$@"
               EOF
               chmod +x $out/bin/run-slides
+
+            ''
+            + lib.optionalString buildHtml ''
+              mkdir -p $html
+              export COLUMNS=120
+              export LINES=40
+              ${presenterm}/bin/presenterm --export-html -c config.yaml ./${slideName}/slides.md
+              cp ./${slideName}/slides.html $html/ || true
             '';
             meta.mainProgram = "run-slides";
           };
+
+        slideNames = [
+          "journee_departement_rob_2026"
+          "nix_workshop_lirmm_2026"
+        ];
+
+        makeBothPackages = slideName: {
+          "${slideName}" = makeSlidePackage slideName false;
+          "${slideName}_html" = makeSlidePackage slideName true;
+        };
+
+        allPackages = builtins.foldl' (acc: slideName: acc // makeBothPackages slideName) { } slideNames;
       in
       {
         rosDistros = [ ];
-        packages = {
-          journee_departement_rob_2026 = makeSlidePackage "journee_departement_rob_2026";
-          nix_workshop_lirmm_2026 = makeSlidePackage "nix_workshop_lirmm_2026";
-        };
+        packages = allPackages;
       }
     );
 }
